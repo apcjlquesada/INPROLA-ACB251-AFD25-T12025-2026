ba9ae092a3e5864e0a91a8ffc6973dda81e6cb02
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-08-07 10:48:50 +0800

    Initial commit

---
+++ week03/Twoinputs.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter two integers");
+
+    // Numerical input
+    int firstnumber = myObj.nextInt();
+    int secondnumber = myObj.nextInt();
+
+
+    // Output input by user
+    System.out.println("First number " + firstnumber); 
+    System.out.println("Second Number " + secondnumber); 
+    
+  }
+}
