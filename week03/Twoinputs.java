3a772194f8bb3a3c97c8417bb3f322ef61042304
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-08-07 03:19:41 +0000

    Updated

--- week03/Twoinputs.java
+++ week03/Twoinputs.java
@@ -1,17 +1,32 @@
-class Main {
+import java.util.Scanner; // import the Scanner class 
+
+class Twoinputs {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
 
     System.out.println("Enter two integers");
 
     // Numerical input
-    int firstnumber = myObj.nextInt();
-    int secondnumber = myObj.nextInt();
+    int a = myObj.nextInt();
+    int a = myObj.nextInt();
 
 
     // Output input by user
-    System.out.println("First number " + firstnumber); 
-    System.out.println("Second Number " + secondnumber); 
+    System.out.println("First number " + a); 
+    System.out.println("Second Number " + b); 
+
+// addition operator
+    System.out.println("a + b = " + (a + b));
+
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
     
   }
 }
