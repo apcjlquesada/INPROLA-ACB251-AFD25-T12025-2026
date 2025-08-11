bad60ee964b17d6bc38ac24e4b3af307b6544840
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-08-11 03:06:59 +0000

    Commit after 5 minutes of coding

--- week04/LoopExercise.java
+++ week04/LoopExercise.java
@@ -1,7 +1,17 @@
 // Program to print a text 5 times
+import java.util.Scanner;  // Import the Scanner class
 
 class Loop01 {
   public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = myObj.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
+
+
+
+
 
     int n = 5;
     // for loop  
