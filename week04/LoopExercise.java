4c5d0881d7201ee6e2cc269d4c63261bbf0f2b0f
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-08-11 03:29:59 +0000

    updates as of the end of 11:30am

--- week04/LoopExercise.java
+++ week04/LoopExercise.java
@@ -6,8 +6,8 @@ class Loop01 {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
     System.out.println("Enter username");
 
-    String userName = myObj.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
+    String Firstnumber = myObj.nextInt());  // Read user input
+    System.out.println("Enter your First number " + userName);  // Output user input
 
 
 
